08e1a88d758efe6f3c15b15c9418a3c7882b28dd
Dave Desrochers <dave.desrochers@aether.com>
2015-10-29 21:08:29 -0700

    Comments in sketch describing initial flow

--- esp8266-thing/prototype/prototype.ino
+++ esp8266-thing/prototype/prototype.ino
@@ -1,12 +1,21 @@
 #define ESP8266_LED 5
 
 void setup() {
+  /* Configure debug */
   pinMode(ESP8266_LED, OUTPUT);
+
+  /* Enable client mode */
+
+  /* connect to router */
 }
 
 void loop() {
+  /* Get requested time interval 'x' from server */
+
+  /* Every 'x' seconds, post the number of pulses */
+  /* Eventually, it should either be sending the flow rate, or telling the server who it is */
   digitalWrite(ESP8266_LED, HIGH);
-  delay(100);
+  delay(150);
   digitalWrite(ESP8266_LED, LOW);
-  delay(100);
+  delay(150);
 }
