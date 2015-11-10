27ea4f776c7af17dd20d91bf16f96ec923d67b60
Dave Desrochers <dave.desrochers@aether.com>
2015-11-09 17:03:16 -0800

    Proper handshake with server for HTTP calls
    We were closing the connection prematurely, so no data was returning

--- esp8266-thing/prototype/prototype.ino
+++ esp8266-thing/prototype/prototype.ino
@@ -6,10 +6,11 @@
 #define WiFiSSID  "Hi_There"
 #define WiFiPSK   "Let me in jambony1"
 
-#define HTTP_PORT 80
-#define SERVER_DOMAIN "www.davesdesrochers.com"
+#define API_PORT 3000
+#define SERVER_DOMAIN "192.168.1.14"
+#define API_SERVER SERVER_DOMAIN ":3000"
 
-#define LOGGING_DOMAIN "192.168.1.250"
+#define LOGGING_DOMAIN "192.168.1.14"
 #define LOGGING_PORT   9393
 
 #define ASSERT(_timeMs_) while(true) busyWait(_timeMs_);
@@ -17,30 +18,30 @@
 static unsigned int timeIntervalInSec = 2;
 static unsigned int totalSec = 0;
 
-void setup() {
+WiFiClient client;
+WiFiClient logClient;
+
+void setup()
+{
   initHardware();
   connectWiFi();
   timeIntervalInSec = getTimeInterval();
 }
 
-void loop() {
+void loop()
+{
   if(totalSec % timeIntervalInSec == 0) {
     /* Post the number of pulses (proportional to flow rate) */
   }
   busyWait(1000);
-  netLog("Testing.");
   ++totalSec;
 }
 
-WiFiClient logClient;
 void netLog(String str)
 {
-  if(!logClient.connected()) {
-    logClient.connect(LOGGING_DOMAIN, LOGGING_PORT);
-  }
-
-  if(logClient.connected()) {
+  if(logClient.connect(LOGGING_DOMAIN, LOGGING_PORT)) {
     logClient.println(str);
+    logClient.stop();
   }
 }
 
@@ -64,6 +65,7 @@ void connectWiFi(void)
     busyWait(100);
   }
   digitalWrite(LED_PIN, LOW);
+  netLog("------ RESET ------");
 }
 
 unsigned int getTimeInterval(void)
@@ -71,37 +73,42 @@ unsigned int getTimeInterval(void)
   uint8_t ledStatus = HIGH;
   String macID = getMacAddr();
 
-  String result = fetchObject("", "?m=" + macID);
+  String result = "π";
 
+  fetchObject(&result, "api/v1/devices/" + macID, "");
+
+  netLog(result);
   /* Parse JSON here */
 
   return result.toInt();
 }
 
-String fetchObject(String path, String params)
+void fetchObject(String *line, String path, String params)
 {
-  String line = "";
-  WiFiClient client;
-
-  if (!client.connect(SERVER_DOMAIN, HTTP_PORT)) {
+  if (!client.connect(SERVER_DOMAIN, API_PORT)) {
     ASSERT(1000);
   }
 
-  String request = "GET /" + path + "?" + params + " HTTP/1.1";
+  String request = "GET /" + path;
+  if(params.length() != 0) {
+    request += "/?" + params;
+  }
+  request += " HTTP/1.1";
+
   client.println(request);
+  client.println("Host: " API_SERVER);
   client.println("User-Agent: good/0.0");
-  client.println("Host: " SERVER_DOMAIN);
   client.println("Accept: */*");
   client.println();
 
-  while(client.available()){
-    line += client.readStringUntil('\r');
+  /* client.setTimeout(10000); */
+  do {
+    (*line) += client.readString();
+    /* client.setTimeout(10); */
     busyWait(10);
-    /* Serial.print(line); */
-  }
-  digitalWrite(LED_PIN, LOW);
+  } while(client.available());
 
-  return line;
+  digitalWrite(LED_PIN, LOW);
 }
 
 void busyWait(unsigned int timeMs)
